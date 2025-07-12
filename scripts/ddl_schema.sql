e83710a797b32bfd84b5f9486e990ba0e74f1850
Swaraj <swaraj8111998@gmail.com>
2025-07-12 17:32:07 +0530

    ADDED SQLDDL

--- scripts/ddl_schema.sql
+++ scripts/ddl_schema.sql
@@ -1,3 +1,7 @@
+USE DataWarehouse;
+
+IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL /*U=OBJECT TYPE USER DEFINED TABLE*/
+    Drop TABLE bronze.crm_cust_info;
 Create table bronze.crm_cust_info(
     cst_id INT,
     cst_firstname NVARCHAR(50),
@@ -7,6 +11,8 @@ Create table bronze.crm_cust_info(
     cst_create_date DATE
 );
 
+IF OBJECT_ID('bronze.prd_info','U') IS NOT NULL 
+    Drop TABLE bronze.prd_info;
 Create table bronze.prd_info(
     prd_id INT,
     prd_key NVARCHAR(50),
@@ -17,6 +23,8 @@ Create table bronze.prd_info(
     prd_end_dt DATE
 );
 
+IF OBJECT_ID('bronze.sales_details','U') IS NOT NULL 
+    Drop TABLE bronze.sales_details;
 Create table bronze.sales_details(
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
@@ -30,17 +38,23 @@ Create table bronze.sales_details(
 );
 
 
+IF OBJECT_ID('bronze.cust_az12','U') IS NOT NULL 
+    Drop TABLE bronze.cust_az12;
 create table bronze.cust_az12(
     CID NVARCHAR(50),
     BDATE DATE,
     GEN NVARCHAR(50)
 );
 
+IF OBJECT_ID('bronze.loc_a101','U') IS NOT NULL 
+    Drop TABLE bronze.loc_a101;
 create table bronze.loc_a101(
     CID NVARCHAR(50),
     CNTRY NVARCHAR(50)
 );
 
+IF OBJECT_ID('bronze.px_cat_g1v2','U') IS NOT NULL 
+    Drop TABLE bronze.px_cat_g1v2;
 create table bronze.px_cat_g1v2(
     ID NVARCHAR(50),
     CAT NVARCHAR(50),
