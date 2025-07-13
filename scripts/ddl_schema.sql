15ad07d9ccf55d48d21a079bd948f6aeada573b5
Swaraj <swaraj8111998@gmail.com>
2025-07-13 09:20:15 +0530

    Added bronze layer loading script

--- scripts/ddl_schema.sql
+++ scripts/ddl_schema.sql
@@ -1,64 +1,92 @@
-USE DataWarehouse;
+Create or alter procedure bronze.load_bronze as
 
-IF OBJECT_ID('bronze.crm_cust_info','U') IS NOT NULL /*U=OBJECT TYPE USER DEFINED TABLE*/
-    Drop TABLE bronze.crm_cust_info;
-Create table bronze.crm_cust_info(
-    cst_id INT,
-    cst_firstname NVARCHAR(50),
-    cst_lastname NVARCHAR(50),
-    cst_marital_status NVARCHAR(50),
-    cst_gndr NVARCHAR(50),
-    cst_create_date DATE
-);
+BEGIN
+    BEGIN TRY
 
-IF OBJECT_ID('bronze.prd_info','U') IS NOT NULL 
-    Drop TABLE bronze.prd_info;
-Create table bronze.prd_info(
-    prd_id INT,
-    prd_key NVARCHAR(50),
-    prd_nm NVARCHAR(50),
-    prd_cost INT,
-    prd_line NVARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt DATE
-);
+        PRINT '===================================================='
+        PRINT 'LOADING BRONZE LAYER'
+        PRINT '===================================================='
+        PRINT '----------------------------------------------------'
+        PRINT 'LOADING  CRM TABLES'
+        PRINT '----------------------------------------------------'
+        PRINT '>>TRUNCATING TABLE:bronze.crm_cust_info'
+        Truncate table bronze.crm_cust_info;
+        PRINT '>>INSERTING DATA INTO: bronze.crm_cust_info'
+        BULK INSERT bronze.crm_cust_info
+        FROM '/var/opt/mssql/cust_info.csv'
+        WITH (
+            FIRSTROW=2, /*since data starts from second row in csv, first one contains only column names*/
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
 
-IF OBJECT_ID('bronze.sales_details','U') IS NOT NULL 
-    Drop TABLE bronze.sales_details;
-Create table bronze.sales_details(
-    sls_ord_num NVARCHAR(50),
-    sls_prd_key NVARCHAR(50),
-    sls_cust_id INT,
-    sls_order_dt DATE,
-    sls_ship_dt INT,
-    sls_due_dt INT,
-    sls_sales INT,
-    sls_quantity INT,
-    sls_price INT
-);
+        PRINT '>>TRUNCATING TABLE:bronze.prd_info'
+        TRUNCATE TABLE bronze.prd_info;
+        PRINT '>>INSERTING DATA INTO: bronze.prd_info'
+        BULK INSERT bronze.prd_info
+        FROM '/var/opt/mssql/prd_info.csv'
+        WITH(
+            FIRSTROW=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
 
+        PRINT '>>TRUNCATING TABLE:bronze.sales_details'
+        Truncate table bronze.sales_details;
+        PRINT '>>INSERTING DATA INTO: bronze.sales_details'
+        bulk insert bronze.sales_details
+        FROM '/var/opt/mssql/sales_details.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+        
+        PRINT '----------------------------------------------------'
+        PRINT 'LOADING  ERP TABLES'
+        PRINT '----------------------------------------------------'
+        PRINT '>>TRUNCATING TABLE:bronze.cust_az12'
+        Truncate table bronze.cust_az12;
+        PRINT '>>INSERTING DATA INTO: bronze.cust_az12'
+        bulk insert bronze.cust_az12
+        FROM '/var/opt/mssql/CUST_AZ12.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
 
-IF OBJECT_ID('bronze.cust_az12','U') IS NOT NULL 
-    Drop TABLE bronze.cust_az12;
-create table bronze.cust_az12(
-    CID NVARCHAR(50),
-    BDATE DATE,
-    GEN NVARCHAR(50)
-);
+        PRINT '>>TRUNCATING TABLE:bronze.loc_a101'
+        Truncate table bronze.loc_a101;
+        PRINT '>>INSERTING DATA INTO: bronze.loc_a101'
+        bulk insert bronze.loc_a101
+        FROM '/var/opt/mssql/LOC_A101.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
 
-IF OBJECT_ID('bronze.loc_a101','U') IS NOT NULL 
-    Drop TABLE bronze.loc_a101;
-create table bronze.loc_a101(
-    CID NVARCHAR(50),
-    CNTRY NVARCHAR(50)
-);
+        PRINT '>>TRUNCATING TABLE:bronze.px_cat_g1v2'
+        Truncate table bronze.px_cat_g1v2;
+        PRINT '>>INSERTING DATA INTO: bronze.px_cat_g1v2'
+        bulk insert bronze.px_cat_g1v2
+        FROM '/var/opt/mssql/PX_CAT_G1V2.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+    END TRY
+    BEGIN CATCH
+        PRINT '===================================================='
+        PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+        PRINT 'ERROR MESSAGE'+ERROR_MESSAGE();
+        PRINT 'ERROR MESSAGE'+CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT 'ERROR MESSAGE' +CAST(ERROR_STATE() AS NVARCHAR);
+        PRINT '===================================================='
 
-IF OBJECT_ID('bronze.px_cat_g1v2','U') IS NOT NULL 
-    Drop TABLE bronze.px_cat_g1v2;
-create table bronze.px_cat_g1v2(
-    ID NVARCHAR(50),
-    CAT NVARCHAR(50),
-    SUBCAT NVARCHAR(50),
-    MAINTENANCE NVARCHAR(50)
-);
+    END CATCH
+END
 
+/*1.40.00*/
