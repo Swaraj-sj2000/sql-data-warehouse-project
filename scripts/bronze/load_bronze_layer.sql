23716c267138c482420c99e5dd345540e380c635
Swaraj <swaraj8111998@gmail.com>
2025-07-13 12:02:47 +0530

    Changed some previous errors in file arranegment

--- scripts/bronze/load_bronze_layer.sql
+++ scripts/bronze/load_bronze_layer.sql
@@ -1,1 +1,123 @@
-EXEC bronze.load_bronze;
+Create or alter procedure bronze.load_bronze as
+BEGIN
+    DECLARE @batch_start DATETIME,@bath_end DATETIME;
+    SET @batch_start=GETDATE();
+
+    BEGIN TRY
+        DECLARE @start_time DATETIME, @end_time DATETIME;
+
+        PRINT '===================================================='
+        PRINT 'LOADING BRONZE LAYER'
+        PRINT '===================================================='
+
+        PRINT '----------------------------------------------------'
+        PRINT 'LOADING  CRM TABLES'
+        PRINT '----------------------------------------------------'
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.crm_cust_info'
+        Truncate table bronze.crm_cust_info;
+        PRINT '>>INSERTING DATA INTO: bronze.crm_cust_info'
+        BULK INSERT bronze.crm_cust_info
+        FROM '/var/opt/mssql/cust_info.csv'
+        WITH (
+            FIRSTROW=2, /*since data starts from second row in csv, first one contains only column names*/
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.prd_info'
+        TRUNCATE TABLE bronze.prd_info;
+        PRINT '>>INSERTING DATA INTO: bronze.prd_info'
+        BULK INSERT bronze.prd_info
+        FROM '/var/opt/mssql/prd_info.csv'
+        WITH(
+            FIRSTROW=2,
+            FIELDTERMINATOR=',',
+            TABLOCK
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.sales_details'
+        Truncate table bronze.sales_details;
+        PRINT '>>INSERTING DATA INTO: bronze.sales_details'
+        bulk insert bronze.sales_details
+        FROM '/var/opt/mssql/sales_details.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+        
+        PRINT '----------------------------------------------------'
+        PRINT 'LOADING  ERP TABLES'
+        PRINT '----------------------------------------------------'
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.cust_az12'
+        Truncate table bronze.cust_az12;
+        PRINT '>>INSERTING DATA INTO: bronze.cust_az12'
+        bulk insert bronze.cust_az12
+        FROM '/var/opt/mssql/CUST_AZ12.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.loc_a101'
+        Truncate table bronze.loc_a101;
+        PRINT '>>INSERTING DATA INTO: bronze.loc_a101'
+        bulk insert bronze.loc_a101
+        FROM '/var/opt/mssql/LOC_A101.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+
+        SET @start_time=GETDATE();
+        PRINT '>>TRUNCATING TABLE:bronze.px_cat_g1v2'
+        Truncate table bronze.px_cat_g1v2;
+        PRINT '>>INSERTING DATA INTO: bronze.px_cat_g1v2'
+        bulk insert bronze.px_cat_g1v2
+        FROM '/var/opt/mssql/PX_CAT_G1V2.csv'
+        with(
+            firstrow=2,
+            fieldterminator=',',
+            tablock
+        );
+        SET @end_time=GETDATE();
+        PRINT '>>LOAD DURATION '+CAST(DATEDIFF(second,@start_time,@end_time) AS NVARCHAR)+'seconds';
+        PRINT '-----------';
+
+    END TRY
+    BEGIN CATCH
+        PRINT '===================================================='
+        PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+        PRINT 'ERROR MESSAGE'+ERROR_MESSAGE();
+        PRINT 'ERROR MESSAGE'+CAST(ERROR_NUMBER() AS NVARCHAR);
+        PRINT 'ERROR MESSAGE' +CAST(ERROR_STATE() AS NVARCHAR);
+        PRINT '===================================================='
+
+    END CATCH
+    SET @bath_end=GETDATE();
+    PRINT '>>TOTAL LOADING TIME: '+CAST(DATEDIFF(second,@batch_start,@bath_end) AS NVARCHAR)+'seconds';
+END
+
+/*1.40.00*/
--- scripts/ddl_schema.sql
+++ scripts/ddl_schema.sql
@@ -1,92 +1,42 @@
-Create or alter procedure bronze.load_bronze as
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates a new database named 'DataWarehouse' after checking if it already exists. 
+    If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas 
+    within the database: 'bronze', 'silver', and 'gold'.
+	
+WARNING:
+    Running this script will drop the entire 'DataWarehouse' database if it exists. 
+    All data in the database will be permanently deleted. Proceed with caution 
+    and ensure you have proper backups before running this script.
+*/
 
+USE master;
+GO
+
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
-    BEGIN TRY
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
 
-        PRINT '===================================================='
-        PRINT 'LOADING BRONZE LAYER'
-        PRINT '===================================================='
-        PRINT '----------------------------------------------------'
-        PRINT 'LOADING  CRM TABLES'
-        PRINT '----------------------------------------------------'
-        PRINT '>>TRUNCATING TABLE:bronze.crm_cust_info'
-        Truncate table bronze.crm_cust_info;
-        PRINT '>>INSERTING DATA INTO: bronze.crm_cust_info'
-        BULK INSERT bronze.crm_cust_info
-        FROM '/var/opt/mssql/cust_info.csv'
-        WITH (
-            FIRSTROW=2, /*since data starts from second row in csv, first one contains only column names*/
-            FIELDTERMINATOR=',',
-            TABLOCK
-        );
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
 
-        PRINT '>>TRUNCATING TABLE:bronze.prd_info'
-        TRUNCATE TABLE bronze.prd_info;
-        PRINT '>>INSERTING DATA INTO: bronze.prd_info'
-        BULK INSERT bronze.prd_info
-        FROM '/var/opt/mssql/prd_info.csv'
-        WITH(
-            FIRSTROW=2,
-            FIELDTERMINATOR=',',
-            TABLOCK
-        );
+USE DataWarehouse;
+GO
 
-        PRINT '>>TRUNCATING TABLE:bronze.sales_details'
-        Truncate table bronze.sales_details;
-        PRINT '>>INSERTING DATA INTO: bronze.sales_details'
-        bulk insert bronze.sales_details
-        FROM '/var/opt/mssql/sales_details.csv'
-        with(
-            firstrow=2,
-            fieldterminator=',',
-            tablock
-        );
-        
-        PRINT '----------------------------------------------------'
-        PRINT 'LOADING  ERP TABLES'
-        PRINT '----------------------------------------------------'
-        PRINT '>>TRUNCATING TABLE:bronze.cust_az12'
-        Truncate table bronze.cust_az12;
-        PRINT '>>INSERTING DATA INTO: bronze.cust_az12'
-        bulk insert bronze.cust_az12
-        FROM '/var/opt/mssql/CUST_AZ12.csv'
-        with(
-            firstrow=2,
-            fieldterminator=',',
-            tablock
-        );
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
 
-        PRINT '>>TRUNCATING TABLE:bronze.loc_a101'
-        Truncate table bronze.loc_a101;
-        PRINT '>>INSERTING DATA INTO: bronze.loc_a101'
-        bulk insert bronze.loc_a101
-        FROM '/var/opt/mssql/LOC_A101.csv'
-        with(
-            firstrow=2,
-            fieldterminator=',',
-            tablock
-        );
+CREATE SCHEMA silver;
+GO
 
-        PRINT '>>TRUNCATING TABLE:bronze.px_cat_g1v2'
-        Truncate table bronze.px_cat_g1v2;
-        PRINT '>>INSERTING DATA INTO: bronze.px_cat_g1v2'
-        bulk insert bronze.px_cat_g1v2
-        FROM '/var/opt/mssql/PX_CAT_G1V2.csv'
-        with(
-            firstrow=2,
-            fieldterminator=',',
-            tablock
-        );
-    END TRY
-    BEGIN CATCH
-        PRINT '===================================================='
-        PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
-        PRINT 'ERROR MESSAGE'+ERROR_MESSAGE();
-        PRINT 'ERROR MESSAGE'+CAST(ERROR_NUMBER() AS NVARCHAR);
-        PRINT 'ERROR MESSAGE' +CAST(ERROR_STATE() AS NVARCHAR);
-        PRINT '===================================================='
-
-    END CATCH
-END
-
-/*1.40.00*/
+CREATE SCHEMA gold;
+GO
